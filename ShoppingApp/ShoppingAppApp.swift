6a05b79b98f6bb2fc76a3b1b0ccf1ef07b9c9415
Luc Kabamba <klucakimana@yahoo.com>
2021-11-26 16:20:38 +0100

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Luc Kabamba on 2021-11-26.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
